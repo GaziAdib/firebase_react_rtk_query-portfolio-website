0d47533a536be9478c3014dd389803c15c70f034
unknown <adibcse10@gmail.com>
2022-11-05 18:43:22 +0600

    added tailwind ui add projects page redesign

--- src/pages/admin/project/AddProject.jsx
+++ src/pages/admin/project/AddProject.jsx
@@ -1,29 +1,20 @@
-import React, {useState} from 'react'
-import { FormControl, TextField, Container } from '@mui/material';
+import React, { useState } from 'react'
 
 // add storage for uploading image data
 import { storage } from '../../../firebase';
 import { getDownloadURL, ref, uploadBytes } from '@firebase/storage';
 
-// add database for data uploading
-// import { database } from '../../../firebase';
-// import {ref as dbRef, set, push } from '@firebase/database';
 
-// import UUID
-
-import { v4 as uuidv4 } from 'uuid'
 import { useAddProjectMutation } from '../../../features/projects/projectsApi';
 import { useNavigate } from 'react-router-dom';
-
+import Error from '../../../components/ui/Error';
 
 
 const AddProject = () => {
 
-    const [addProject] = useAddProjectMutation();
+    const [addProject, { isLoading, isError, error }] = useAddProjectMutation();
 
     const navigate = useNavigate();
-
-    var id = uuidv4()
 
     const [progress, setProgress] = useState(0)
     const [pUrl, setUrl] = useState(null)
@@ -37,7 +28,7 @@ const AddProject = () => {
     const [projectDemoLink, setProjectDemoLink] = useState('')
 
     const clearData = () => {
-       
+
         setProjectTitle('')
         setProjectImageUrl(null)
         setProjectTag('')
@@ -45,15 +36,14 @@ const AddProject = () => {
         setProjectVideoLink('')
         setProjectGitLink('')
         setProjectDemoLink('')
-      
+
     }
 
-    const formHandler = (e) => {
-        e.preventDefault() 
+    const formSubmitHandler = (e) => {
+        e.preventDefault()
 
-        if(pUrl !== '') {
+        if (pUrl !== '') {
             addProject({
-                id: uuidv4(),
                 projectImageUrl: pUrl,
                 projectTitle: projectTitle,
                 projectTag: projectTag,
@@ -85,41 +75,98 @@ const AddProject = () => {
 
     return (
         <>
-            <h1>Add Project</h1>
-            <hr />
 
-            <Container>
-                <form onSubmit={formHandler} encType="multipart/form-data">
-                    <FormControl>
+            <div className="max-w-2xl bg-white py-10 px-5 m-auto w-full mt-10">
 
-                        <input
-                            accept="image/*"
-                            type="file"
-                            onChange={fileHandler}
-                        />
+                <div className="text-3xl mb-6 text-center ">
+                    Add Projects To Your Liking ❤️
+                </div>
+                <hr />
+                <br />
 
-                        
-                        <TextField required  style={{margin: '5px', padding:'5px'}} id="outlined-basic" label="Project Title" variant="outlined" value={projectTitle} onChange={(e) => setProjectTitle(e.target.value)} />
-                        <TextField required  style={{margin: '5px', padding:'5px'}} id="outlined-basic" label="Project Tag" variant="outlined" value={projectTag} onChange={(e) => setProjectTag(e.target.value)} />
-                        <TextField required  style={{margin: '5px', padding:'5px'}} id="outlined-basic" label="Project Description" variant="outlined" multiline value={projectDescription} onChange={(e) => setProjectDescription(e.target.value)} />
-                        <TextField required  style={{margin: '5px', padding:'5px'}} id="outlined-basic" label="Project GitHub Link" variant="outlined" value={projectGitLink} onChange={(e) => setProjectGitLink(e.target.value)} />
-                        <TextField required  style={{margin: '5px', padding:'5px'}} id="outlined-basic" label="Project Video Link" variant="outlined" value={projectVideoLink} onChange={(e) => setProjectVideoLink(e.target.value)} />
-                        <TextField required  style={{margin: '5px', padding:'5px'}} id="outlined-basic" label="Project Demo Link" variant="outlined" value={projectDemoLink} onChange={(e) => setProjectDemoLink(e.target.value)} />
+                <form onSubmit={formSubmitHandler}>
+                    <div className="grid grid-cols-2 gap-4 max-w-xl m-auto">
 
-                        <button type="submit">Submit</button>
-                    </FormControl>
-                
+                        <div className="col-span-2">
+                            <input
+                                accept="image/*"
+                                type="file"
+                                onChange={fileHandler}
+                            />
+                        </div>
+
+                        <div className="col-span-2 lg:col-span-1">
+                            <input type="text" value={projectTitle} onChange={(e) => setProjectTitle(e.target.value)} className="rounded-lg border-solid border-slate-400 border-2 p-3 md:text-xl w-full" required placeholder="Title" />
+                        </div>
+
+                        <div className="col-span-2 lg:col-span-1">
+                            <input type="text" value={projectTag} onChange={(e) => setProjectTag(e.target.value)} className="rounded-lg border-solid border-slate-400 border-2 p-3 md:text-xl w-full" required placeholder="Tags" />
+                        </div>
+
+                        <div className="col-span-2">
+                            <textarea cols="30" rows="8" value={projectDescription} onChange={(e) => setProjectDescription(e.target.value)} className="rounded-lg border-dashed border-slate-400 border-2 p-3 md:text-xl w-full" required placeholder="Description"></textarea>
+                        </div>
+
+                        <div className="col-span-2">
+                            <input type="text" value={projectGitLink} onChange={(e) => setProjectGitLink(e.target.value)} className="rounded-lg border-solid border-slate-400 border-2 p-3 md:text-xl w-full" required placeholder="Git Link" />
+                        </div>
+
+                        <div className="col-span-2">
+                            <input type="text" value={projectVideoLink} onChange={(e) => setProjectVideoLink(e.target.value)} className="rounded-lg border-solid border-slate-400 border-2 p-3 md:text-xl w-full" required placeholder="Video Link" />
+                        </div>
+
+                        <div className="col-span-2">
+                            <input type="text" value={projectDemoLink} onChange={(e) => setProjectDemoLink(e.target.value)} className="rounded-lg border-solid border-slate-400 border-2 p-3 md:text-xl w-full" required placeholder="Demo Link" />
+                        </div>
+
+                        <div className="col-span-2 text-right">
+                            <button disabled={isLoading ? isLoading : undefined} className="rounded-lg py-3 px-6 bg-green-500 text-white font-bold w-full sm:w-32 bg-gradient-to-r from-indigo-500 via-green-500 to-pink-500 hover:from-pink-500 hover:to-yellow-500">
+                                Add Project
+                            </button>
+                        </div>
+
+                    </div>
                 </form>
+                <div className="flex items-center justify-between">
+                    {!isLoading && error && <Error message={error} />}
+                </div>
+            </div>
 
-                {/* <br/>
 
-                <h3>Uploaded {progress} %</h3> */}
-            </Container>
 
-            
-            
         </>
     )
 }
 
 export default AddProject
+
+// <h1>Add Project</h1>
+// <hr />
+
+// <Container>
+//     <form onSubmit={formHandler} encType="multipart/form-data">
+//         <FormControl>
+
+//             <input
+//                 accept="image/*"
+//                 type="file"
+//                 onChange={fileHandler}
+//             />
+
+
+//             <TextField required style={{ margin: '5px', padding: '5px' }} id="outlined-basic" label="Project Title" variant="outlined" value={projectTitle} onChange={(e) => setProjectTitle(e.target.value)} />
+//             <TextField required style={{ margin: '5px', padding: '5px' }} id="outlined-basic" label="Project Tag" variant="outlined" value={projectTag} onChange={(e) => setProjectTag(e.target.value)} />
+//             <TextField required style={{ margin: '5px', padding: '5px' }} id="outlined-basic" label="Project Description" variant="outlined" multiline value={projectDescription} onChange={(e) => setProjectDescription(e.target.value)} />
+//             <TextField required style={{ margin: '5px', padding: '5px' }} id="outlined-basic" label="Project GitHub Link" variant="outlined" value={projectGitLink} onChange={(e) => setProjectGitLink(e.target.value)} />
+//             <TextField required style={{ margin: '5px', padding: '5px' }} id="outlined-basic" label="Project Video Link" variant="outlined" value={projectVideoLink} onChange={(e) => setProjectVideoLink(e.target.value)} />
+//             <TextField required style={{ margin: '5px', padding: '5px' }} id="outlined-basic" label="Project Demo Link" variant="outlined" value={projectDemoLink} onChange={(e) => setProjectDemoLink(e.target.value)} />
+
+//             <button type="submit">Submit</button>
+//         </FormControl>
+
+//     </form>
+
+//     {/* <br/>
+
+//     <h3>Uploaded {progress} %</h3> */}
+// </Container>
