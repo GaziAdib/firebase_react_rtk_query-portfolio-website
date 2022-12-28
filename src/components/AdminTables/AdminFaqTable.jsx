217db737767dd0bfb870e32ccb916c68a8204aa5
unknown <adibcse10@gmail.com>
2022-12-28 21:35:46 +0600

    added Admin Section Add and Delete OF FAQ Module

---
+++ src/components/AdminTables/AdminFaqTable.jsx
@@ -0,0 +1,86 @@
+import React from 'react';
+import TrashIcon from '../../assets/images/trash.png';
+import AddIcon from '../../assets/images/edit.png';
+import { Link } from 'react-router-dom';
+import { useDeleteFaqMutation } from '../../features/faqs/faqsApi';
+
+
+
+
+const AdminFaqTable = ({ faqs }) => {
+
+    const [deleteFaq, { isError, isLoading, error }] = useDeleteFaqMutation() || {};
+
+    const deleteFaqHandler = (id) => {
+
+        // delete faq section from admin
+        deleteFaq(id);
+
+    }
+
+    return (
+        <>
+
+            <div class="overflow-x-auto relative shadow-lg shadow-md sm:rounded-lg">
+                <table class="w-full text-sm text-left text-gray-500 dark:text-gray-400">
+                    <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+                        <tr>
+
+                            <th scope="col" class="py-3 px-6">
+                                FAQ Question
+                            </th>
+                            <th scope="col" class="py-3 px-6">
+                                FAQ Answer
+                            </th>
+
+                            <th scope="col" class="py-3 px-6">
+                                Add
+                            </th>
+                            <th scope="col" class="py-3 px-6">
+                                Delete
+                            </th>
+                        </tr>
+                    </thead>
+                    <tbody>
+
+                        {faqs?.length > 0 ? (faqs?.map((faq, index) => {
+
+                            const { key, faqQuestion, faqAnswer } = faq || {};
+
+                            return <tr key={index} class="bg-white border-b dark:bg-gray-900 dark:border-gray-700">
+
+                                <td class="py-4 px-6 dark:text-white font-semibold">
+                                    {faqQuestion}
+                                </td>
+
+                                <td class="py-4 px-6 dark:text-white font-semibold">
+                                    {
+                                        faqAnswer?.length > 100 ? faqAnswer.slice(0, 99) : faqAnswer
+                                    }
+                                </td>
+
+                                <td class="py-4 px-6">
+                                    <Link to={'/addFaq'} class="font-medium text-blue-600 dark:text-blue-500 hover:underline"><img src={AddIcon} height="30px" width="30px" alt="add" /></Link>
+                                </td>
+
+                                <td class="py-4 px-6">
+                                    <button class="font-medium text-blue-600 dark:text-blue-500 hover:underline hover:bg-red-400 rounded " onClick={() => deleteFaqHandler(key)}><img src={TrashIcon} height="30px" width="30px" alt="trash" /></button>
+                                </td>
+
+
+                            </tr>
+                        })
+
+                        ) : ('no data in Faq table')}
+
+
+
+                    </tbody>
+                </table>
+            </div>
+
+        </>
+    )
+}
+
+export default AdminFaqTable
--- src/pages/admin/dashboard/Dashboard.jsx
+++ src/pages/admin/dashboard/Dashboard.jsx
@@ -3,12 +3,14 @@ import AdminAboutTable from '../../../components/AdminTables/AdminAboutTable';
 import AdminAchievementTable from '../../../components/AdminTables/AdminAchievementTable';
 import AdminCourseTable from '../../../components/AdminTables/AdminCourseTable';
 import AdminExperienceTable from '../../../components/AdminTables/AdminExperienceTable';
+import AdminFaqTable from '../../../components/AdminTables/AdminFaqTable';
 import AdminProjectTable from '../../../components/AdminTables/AdminProjectTable';
 import AdminSkillTable from '../../../components/AdminTables/AdminSkillTable';
 import { useFetchAboutsQuery } from '../../../features/abouts/aboutsApi';
 import { useFetchAchievementsQuery } from '../../../features/achievements/achievementsApi';
 import { useFetchCoursesQuery } from '../../../features/courses/coursesApi';
 import { useFetchExperiencesQuery } from '../../../features/experiences/experiencesApi';
+import { useFetchFaqsQuery } from '../../../features/faqs/faqsApi';
 import { useFetchProjectsQuery } from '../../../features/projects/projectsApi';
 import { useFetchSkillsQuery } from '../../../features/skills/skillsApi';
 
@@ -16,54 +18,60 @@ const Dashboard = () => {
 
   // GET Required Data from Projects Database
 
-  const {data: projects, error: projectError, isLoading: projectIsLoading} = useFetchProjectsQuery() || {};
+  const { data: projects, error: projectError, isLoading: projectIsLoading } = useFetchProjectsQuery() || {};
 
-  const {data: courses, error: courseError, isLoading: courseIsLoading} = useFetchCoursesQuery() || {};
+  const { data: courses, error: courseError, isLoading: courseIsLoading } = useFetchCoursesQuery() || {};
 
-  const {data: skills, error: skillError, isLoading: skillIsLoading} = useFetchSkillsQuery() || {};
+  const { data: skills, error: skillError, isLoading: skillIsLoading } = useFetchSkillsQuery() || {};
 
-  const {data: abouts, error: aboutError, isLoading: aboutIsLoading} = useFetchAboutsQuery() || {};
+  const { data: abouts, error: aboutError, isLoading: aboutIsLoading } = useFetchAboutsQuery() || {};
 
-  const {data: achievements, error: achievementError, isLoading: achievementIsLoading} = useFetchAchievementsQuery() || {};
+  const { data: achievements, error: achievementError, isLoading: achievementIsLoading } = useFetchAchievementsQuery() || {};
 
-  const {data: experiences, error: experienceError, isLoading: experienceIsLoading} = useFetchExperiencesQuery() || {};
+  const { data: experiences, error: experienceError, isLoading: experienceIsLoading } = useFetchExperiencesQuery() || {};
+
+  const { data: faqs, error: faqError, isLoading: faqIsLoading } = useFetchFaqsQuery() || {};
+
 
   return (
     <>
-    <div className='bg-gradient-to-r from-cyan-500 to-blue-500 h-screen h-full'>
+      <div className='bg-gradient-to-r from-cyan-500 to-blue-500 h-screen h-full'>
 
-    <h1 className='text-lg text-center underline font-bold text-white m-2 px-2 py-2'>Admin Dashboard</h1>
-      <br />
+        <h1 className='text-lg text-center underline font-bold text-white m-2 px-2 py-2'>Admin Dashboard</h1>
+        <br />
 
-      <div className='container mx-auto px-2 py-3'>
+        <div className='container mx-auto px-2 py-3'>
 
-        {/* // table 1 */}
+          {/* // table 1 */}
 
-        <h2 className='text-lg text-center font-bold text-white m-2 px-2 py-2'>My Projects</h2>
-         <AdminProjectTable projects={projects} />
-         <br />
-         <h2 className='text-lg text-center font-bold text-white m-2 px-2 py-2'>My Courses</h2>
-         <AdminCourseTable courses={courses} />
-         <br />
-         <h2 className='text-lg text-center font-bold text-white m-2 px-2 py-2'>My Skills</h2>
-         <AdminSkillTable skills={skills} />
-         <br />
-         <h2 className='text-lg text-center font-bold text-white m-2 px-2 py-2'>My Abouts</h2>
-         <AdminAboutTable abouts={abouts} />
-         <br />
-         <h2 className='text-lg text-center font-bold text-white m-2 px-2 py-2'>My Achievements</h2>
-         <AdminAchievementTable achievements={achievements} />
-         <br />
-         <h2 className='text-lg text-center font-bold text-white m-2 px-2 py-2'>Work Experience</h2>
-         <AdminExperienceTable experiences={experiences} />
-         
-      
+          <h2 className='text-lg text-center font-bold text-white m-2 px-2 py-2'>My Projects</h2>
+          <AdminProjectTable projects={projects} />
+          <br />
+          <h2 className='text-lg text-center font-bold text-white m-2 px-2 py-2'>My Courses</h2>
+          <AdminCourseTable courses={courses} />
+          <br />
+          <h2 className='text-lg text-center font-bold text-white m-2 px-2 py-2'>My Skills</h2>
+          <AdminSkillTable skills={skills} />
+          <br />
+          <h2 className='text-lg text-center font-bold text-white m-2 px-2 py-2'>My Abouts</h2>
+          <AdminAboutTable abouts={abouts} />
+          <br />
+          <h2 className='text-lg text-center font-bold text-white m-2 px-2 py-2'>My Achievements</h2>
+          <AdminAchievementTable achievements={achievements} />
+          <br />
+          <h2 className='text-lg text-center font-bold text-white m-2 px-2 py-2'>Work Experience</h2>
+          <AdminExperienceTable experiences={experiences} />
+
+          <h2 className='text-lg text-center font-bold text-white m-2 px-2 py-2'>FAQ Section</h2>
+          <AdminFaqTable faqs={faqs} />
+
+
+        </div>
+
       </div>
-   
-    </div>
-      
-        
-    </>   
+
+
+    </>
   )
 }
 
